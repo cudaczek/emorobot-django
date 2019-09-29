87294d179dd5449a4d5c92be6d492a307154efd4
Magdalena Cudak <magdalena.cudak@gmail.com>
2019-09-29 18:52:16 +0200

    Mock time statistics monitor

--- emorobot/monitor/urls.py
+++ emorobot/monitor/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('api/data/', views.get_data, name='data'),
     path('current/api/data/', views.get_current_data, name='current_data'),
+    path('preview/api/data/', views.get_preview_data, name='preview_data'),
     path('api/chart/data/', views.ChartData.as_view(), name='chart'),
     path('current/', views.current_stats, name='current_stats'),
+    path('preview/', views.preview_stats, name='preview'),
     path('', views.index, name='index'),
 ]
--- emorobot/monitor/views.py
+++ emorobot/monitor/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
@@ -10,6 +12,10 @@ User = get_user_model()
 
 def index(request):
     return render(request, "index.html", {"happy": 101})
+
+
+def preview_stats(request):
+    return render(request, "time_stats.html")
 
 
 def current_stats(request):
@@ -51,6 +57,40 @@ def get_current_data(request, *args, **kwargs):
                          "video_recognizer_labels": list(video_recognizer.keys()),
                          "video_recognizer_data": list(video_recognizer.values()),
                          })  # http response
+
+
+def get_preview_data(request, *args, **kwargs):
+    audio_recognizer = {
+        "female_angry": 1.456557,
+        "female_calm": 3.3254342,
+        "female_fearful": 12.232114,
+        "female_happy": 1.12341e-5,
+        "female_sad": -1.7,
+        "male_angry": 2.43564,
+        "male_calm": 1.234,
+        "male_fearful": 3.5464,
+        "male_happy": 7.23425,
+        "male_sad": 2.1234
+    }
+    video_recognizer = {
+        "angry": 3.1,
+        "disgust": 5.777,
+        "fear": 2.0001,
+        "happy": 0.756,
+        "sad": -1.97,
+        "suprise": 10.56,
+        "neutral": 0.899
+    }
+    date = "2019-09-29"
+    return JsonResponse({"time_stats": [
+        {
+            "t": datetime.datetime.strptime(date, '%Y-%m-%d'),
+            "x": 100
+        }
+    ],
+        "audio_recognizer_labels": list(audio_recognizer.keys()),
+        "video_recognizer_labels": list(video_recognizer.keys()),
+    })  # http response
 
 
 class ChartData(APIView):
