7f8be220b1e5084438c3a5523b787935e11e1237
Aleksandra Drewnik <aleksandra.drewnik@gmail.com>
2019-10-03 23:00:52 +0200

    initial update receiving

--- emorobot/monitor/apps.py
+++ emorobot/monitor/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from . import msq_receiver
 
 
 class MonitorConfig(AppConfig):
     name = 'monitor'
+    def ready(self):
+        self.receiver=msq_receiver.MessageReceiver("emorobo.mqtt.example.topic", "broker.hivemq.com:1883")
