7752251fdae34dbe4cd5e92bb9b74460f263bed4
Magdalena Cudak <magdalena.cudak@gmail.com>
2019-11-29 12:52:11 +0100

    Add html tabs; clean templates

--- emorobot/monitor/urls.py
+++ emorobot/monitor/urls.py
@@ -4,10 +4,8 @@ from .views import ControlPanelView, ConfigFormView, SavingFormView
 from . import views
 
 urlpatterns = [
-    path('api/data/', views.get_data, name='data'),
     path('current/api/data/', views.get_current_data, name='current_data'),
     path('preview/api/data/', views.get_preview_data, name='preview_data'),
-    path('api/chart/data/', views.ChartData.as_view(), name='chart'),
     path('current/', views.current_stats, name='current_stats'),
     path('preview/', views.preview_stats, name='preview'),
     path('control/', ControlPanelView.as_view(), name='control_panel'),
