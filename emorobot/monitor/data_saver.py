2e37366e669226dd2e18daf71b7eb39f1e06c736
Magdalena Cudak <magdalena.cudak@gmail.com>
2019-12-04 18:50:25 +0100

    Sve audio wav

--- emorobot/monitor/data_saver.py
+++ emorobot/monitor/data_saver.py
@@ -1,9 +1,13 @@
 import io
 import os
+import struct
 from datetime import datetime
 
+import numpy as np
 import pandas as pd
 from PIL import Image
+import soundfile as sf
+from librosa.output import write_wav
 
 
 class DataSaver:
@@ -69,8 +73,12 @@ class DataSaver:
         image.save(file_path, "PNG")
 
     def save_audio(self, bytes):
-        # TODO
-        pass
+        if bytes != b'':
+            count = int(len(bytes) / 4)
+            floats = struct.unpack(">" + ('f' * count), bytes)
+            timestamp = datetime.timestamp(datetime.now())
+            file_path = os.path.join(self.directory_path, str(int(timestamp)) + ".wav")
+            sf.write(file_path, np.array(floats), 44100, 'PCM_24', endian='LITTLE') # BIG and CPU ENDIAN do not work - FILE or LITTLE ok
 
     def get_video_labels(self):
         column_names = list(self.video_df.columns.values)
